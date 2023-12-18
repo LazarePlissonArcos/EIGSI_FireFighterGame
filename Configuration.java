f00c9d37c6920786e890daad32031102972f7f5e
LazarePA <lazarepa@protonmail.com>
2023-12-18 17:47:42 +0800

    translated in english

--- Configuration.java
+++ Configuration.java
@@ -1,46 +1,43 @@
-/** Définition de constantes utilisées par le programme.
- * @author Thomas Raimbault
- * @version 2023/01
- */
+// Definition of constants used by the program.
 
 public final class Configuration {
 
-	/* Légende */
+	  /* Legend */
 
-	// Légende au format CSV pour correspondance entre : 
-	// (1) caractère dans fichier texte à charger/enregistrer,
-	// (2) entier dans la Matrice (pour la manipulation des données),
-	// (3) image dans la fenêtre grahique.
+    // CSV legend for correspondence between:
+    // (1) character in the text file to load/save,
+    // (2) integer in the Matrix (for data manipulation),
+    // (3) image in the graphical window.
 	public static String LEGENDE_FICHIER_CSV 	= "legende.csv";
 	public static String LEGENDE_SEPARATEUR 	= ",";
 
 	
-	/* Fenêtre */ 
+	/* Window */ 
 
-	// titre de la fenêtre
+	// Window title
 	public static String TITRE = "Carte";
 
-	// taille pour chaque image (image carrée, largeur = hauteur)
+	// Size for each image (square image, width = height)
 	public static int IMAGE_TAILLE = 50; // en pixels
 
-	// si image inexistante ou inconnue depuis la légende
+	// If the image is missing or unknown according to the legend
 	public static String IMAGE_INCONNUE = "images/inconnu.jpg";
 
-	// zones de messages
-	public static int LARGEUR_INFORMATION_DROITE = 300; // en pixels
-	public static int HAUTEUR_INFORMATION_BAS    = 30;  // en pixels
+	// Message areas
+	public static int LARGEUR_INFORMATION_DROITE = 300; // in pixels
+	public static int HAUTEUR_INFORMATION_BAS    = 30;  // in pixels
 
-	// espacement entre les éléments si graphiquement trop compactés
-	public static int ESPACEMENT_ENTRE_LIGNES = 0;   // en pixels
-	public static int ESPACEMENT_ENTRE_COLONNES = 0; // en pixels
+	// Spacing between elements if graphically too compact
+	public static int ESPACEMENT_ENTRE_LIGNES = 0;   // in pixels
+	public static int ESPACEMENT_ENTRE_COLONNES = 0; // in pixels
 
-	// ajustement si composants graphiques trop compactés
-	public static int AJUSTEMENT_HAUTEUR = 0; // en pixels
-	public static int AJUSTEMENT_LARGEUR = 0; // en pixels
+	// Adjustment if graphical components are too compact
+	public static int AJUSTEMENT_HAUTEUR = 0; // in pixels
+	public static int AJUSTEMENT_LARGEUR = 0; // in pixels
 
-	// décalage au démarrage par rapport au bord supérieur gauche de l'écran (non applicable avec Replit)
-	public static int DECALAGE_HAUT   = 0; // en pixels
-	public static int DECALAGE_GAUCHE = 0; // en pixels
+	// Offset at startup relative to the top-left corner of the screen (not applicable with Replit)
+	public static int DECALAGE_HAUT   = 0; // in pixels
+	public static int DECALAGE_GAUCHE = 0; // in pixels
 
 	
 }
